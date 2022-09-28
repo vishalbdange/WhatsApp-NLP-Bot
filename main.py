dd1d96f4f1f129d34d42dd7a25facb4eb9325b40
Keval Dhanani <dhanani.kev01@gmail.com>
2022-09-28 11:42:20 +0530

    Dialogflow Integration Completed

--- main.py
+++ main.py
@@ -1,26 +1,77 @@
-#import the Client function from the helper library
+# import flask for setting up the web server
+from flask import Flask, request
+
+# import the Client function from the helper library
 from twilio.rest import Client
 
-#import OS for environment variables
+# import Messaging Response for responding to incoming messages
+from twilio.twiml.messaging_response import MessagingResponse
+
+# import OS for environment variables
 import os
 
 # import dotenv for loading the environment variables
 from dotenv import load_dotenv
 load_dotenv()
 
-#set the User credentials
+# import dialogflow for adding conversational AI
+import dialogflow
+from google.api_core.exceptions import InvalidArgument
+
+# creating the Flask object
+app = Flask(__name__)
+
+# set Google Application credentials
+os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = 'dialogflow_private_key.json' # absolute path of JSON file
+
+# set Twilio user credentials
 account_sid = os.environ['TWILIO_ACCOUNT_SID']
 auth_token = os.environ['TWILIO_AUTH_TOKEN']
 phone_number = os.environ['YOUR_WHATSAPP_NUMBER']
 client = Client(account_sid, auth_token)
 
-def main():
-    #send text message
+# set Dialogflow project credentials
+DIALOGFLOW_PROJECT_ID = os.environ['DIALOGFLOW_PROJECT_ID']
+DIALOGFLOW_LANGUAGE_CODE = 'en'
+SESSION_ID = os.environ['SESSION_ID']
+session_client = dialogflow.SessionsClient()
+session = session_client.session_path(DIALOGFLOW_PROJECT_ID, SESSION_ID)
+
+def send_message(message_body):
+    # send text message from bot to user
     text_message = client.messages.create(
-        body='Hello there! I am the WhatsApp Bot!!',
+        body=message_body,
         from_='whatsapp:+14155238886',
         to='whatsapp:+91'+phone_number
     )
 
-if __name__ == '__main__':
-    main()
+@app.route('/message', methods=['GET', 'POST'])
+def message():
+    text_to_be_analyzed = "Cricket"
+    text_input = dialogflow.types.TextInput(text=text_to_be_analyzed, language_code=DIALOGFLOW_LANGUAGE_CODE)
+    query_input = dialogflow.types.QueryInput(text=text_input)
+    try:
+        response = session_client.detect_intent(session=session, query_input=query_input)
+    except InvalidArgument:
+        raise   
+
+def respond(message):
+    response = MessagingResponse()
+    response.message(message)
+    return str(response)
+
+@app.route('/reply', methods=['POST'])
+def reply():
+    message = request.form.get('Body').lower()
+    if message:
+        text_input = dialogflow.types.TextInput(text=message, language_code=DIALOGFLOW_LANGUAGE_CODE)
+        query_input = dialogflow.types.QueryInput(text=text_input)
+        try:
+            response = session_client.detect_intent(session=session, query_input=query_input)
+            # print("Query text:", response.query_result.query_text)
+            # print("Detected intent:", response.query_result.intent.display_name)
+            # print("Detected intent confidence:", response.query_result.intent_detection_confidence)
+            # print("Fulfillment text:", response.query_result.fulfillment_text)
+            return respond(response.query_result.fulfillment_text)
+        except InvalidArgument:
+            raise
